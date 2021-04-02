10f449ecaf7c7ec09ec49b382634cda404ae6eae
aquaclara <>
2021-04-02 14:39:39 +0900

    Revert "Get rid of"
    This reverts commit e73fe8257dab3419a5c27e02088d7d1607623277.

--- dist/script.js
+++
@@ -1,7 +0,0 @@
-// Register service worker to control making site work offline
-
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker
-    .register('/pwa-examples/dist/sw.js')
-    .then(() => { console.log('Service Worker Registered'); });
-}
